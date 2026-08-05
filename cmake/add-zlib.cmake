f8e130f7a09a1f3454d28069264324742fecc215
Allan K. Koech <allankoech.ke@gmail.com>
2026-08-05 23:18:43 +0300

    feat: enable zlib install for drogon discovery

--- cmake/add-zlib.cmake
+++ cmake/add-zlib.cmake
@@ -1,7 +1,7 @@
 set(CMAKE_POSITION_INDEPENDENT_CODE ON)
 set(ZLIB_BUILD_SHARED OFF CACHE BOOL "")
 set(ZLIB_BUILD_TESTING OFF CACHE BOOL "")
-set(ZLIB_INSTALL OFF CACHE BOOL "")
+set(ZLIB_INSTALL ON CACHE BOOL "")
 set(ZLIB_BUILD_EXAMPLES OFF CACHE BOOL "" FORCE)
 add_subdirectory(${CMAKE_CURRENT_SOURCE_DIR}/3rdParty/zlib EXCLUDE_FROM_ALL)
 
@@ -9,7 +9,7 @@ set(ZLIB_ROOT "${CMAKE_SOURCE_DIR}/3rdParty/zlib")
 set(ZLIB_INCLUDE_DIRS "${CMAKE_SOURCE_DIR}/3rdParty/zlib/include")
 set(ZLIB_LIBRARIES "${CMAKE_SOURCE_DIR}/3rdParty/zlib/lib")
 
-target_link_libraries(mantisbase PRIVATE ZLIB::ZLIBSTATIC)
+target_link_libraries(mantisbase PUBLIC ZLIB::ZLIBSTATIC)
 target_include_directories(mantisbase PRIVATE
         "${CMAKE_CURRENT_SOURCE_DIR}/3rdParty/zlib"
         "${CMAKE_CURRENT_BINARY_DIR}/3rdParty/zlib")
